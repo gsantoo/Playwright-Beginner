246009779a5bece5dc5bbc6e574fb5953c566aea
Santosh Geernur <gsantoo@gmail.com>
2026-03-24 12:24:24 -0700

    only test files

--- tests/hilton.com.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.hilton.com/en/');
-  await page.getByRole('link', { name: 'Go Back' }).click();
-});
